7dc523c9b5334a96eba974bf4d6188f8153468f5
Patricia Padilha <patriciapadilhabio@gmail.com>
2022-06-29 14:54:42 -0300

    Iniciando projeto

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const app = require('./app');
 require('dotenv').config();
 
 // não altere esse arquivo, essa estrutura é necessária para à avaliação do projeto
+// Iniciando Projeto
 
 app.listen(process.env.PORT, () => {
   console.log(`Escutando na porta ${process.env.PORT}`);
